446bfdf6a9cbae95b102ebe36ac21b0e2c9f8b19
vianaedu <vianaedu@bol.com.br>
2021-03-08 14:58:00 -0300

    9

--- index.php
+++ index.php
@@ -84,8 +84,8 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container" data-aos="fade-up">
-      <h1>Welcome to Squad</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <h1>Eduardo Barboza</h1>
+      <h2>Aprendendo utilização de modelos com Bootstrap</h2>
       <a href="#about" class="btn-get-started scrollto"><i class="bx bx-chevrons-down"></i></a>
     </div>
   </section><!-- End Hero -->
