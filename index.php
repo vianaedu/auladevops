76db21d166ce33a9cec264b9834692486e3257ed
vianaedu <vianaedu@bol.com.br>
2021-03-08 14:54:58 -0300

    8

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
       <div class="logo">
-        <h1 class="text-light"><a href="index.html"><span>Squadfree</span></a></h1>
+        <h1 class="text-light"><a href="index.html"><span>Eduardo Barboza</span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="https://bootstrapmade.com/demo/templates/Squadfree/assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
